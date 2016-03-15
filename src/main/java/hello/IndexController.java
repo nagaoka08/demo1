2f6333a6d8df658717850e737d69a69334469fa4
naoki08 <nnn.0804.naoki@gmail.com>
2016-03-15 14:27:31 +0900

    first commit2

--- src/main/java/hello/IndexController.java
+++ src/main/java/hello/IndexController.java
@@ -1,12 +1,25 @@
 package hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;;
+
 @Controller
 public class IndexController {
 	
 		@RequestMapping("/")
 		public String index() {
+			System.out.println("hello");
 			return"index";
+			
 		}
+		@RequestMapping(value="/post", method=RequestMethod.POST)
+		public String index1() {
+			System.out.println("hello");
+			
+	        return "index";
+	    }
+
 	}
